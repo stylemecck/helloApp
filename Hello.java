a6f677d92b64d5986756c93796fb21355d691c0f
stylemecck <satyamkashyapoffical@gmail.com>
2024-03-08 19:33:01 +0530

    first

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+    public static void main(String args [] ) {
+        // System.out.println("Google");
+        // System.out.print("1 2 3 4 \n");
+        // System.out.print("Satyam World");
+        
+    }
+}
+//Boiler plate code
